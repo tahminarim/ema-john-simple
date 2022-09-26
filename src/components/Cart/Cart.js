e35bde5d54ebe7c8855a3797efc8442be832ba81
tahminarim <tahminarim9@gmail.com>
2022-09-26 11:22:04 +0200

    second

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = ({cart}) => {
+    return (
+        <div>
+             <h2> Order Summary </h2>
+                <p> Selected items: {cart.length}</p>
+          
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,29 @@
+//import React, { useState } from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+//const {handleAddToCart , product} = props;
+    const  {name, img, seller, price, ratings} = props.product;
+
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='product-name'>{name} </p>
+            <p> Price: ${price}</p>
+            <p> <small> Seller: {seller}</small></p>
+            <p> <small> Ratings: {ratings} start</small></p>
+            </div>
+            <button onClick={()=> props.handleAddToCart(props.product)} className='btn-cart'>
+                <p > Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart} > </FontAwesomeIcon>
+            </button>
+
+        </div>
+    );
+};
+
+export default Product;
